75299da7be99f1ec8921846fd45473528aeb35d6
Ilya Volkov <illvolkov@bk.ru>
2022-06-02 08:39:45 +0500

    Create CategoryCollectionViewCellType

---
+++ AppleMusicSwiftUI/Sources/View/SearchView/UIKitCategoryView/CategoryCollectionViewCellType.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryCollectionViewCellType.swift
+//  AppleMusicSwiftUI
+//
+//  Created by Ilya Volkov on 02.06.2022.
+//
+
+import UIKit
+
+final class SearchCollectionViewCellType: UICollectionViewCell {
+    
+    static let identifier = "SearchCollectionViewCell"
+    
+    private lazy var categoryImage: UIImageView = {
+        let image = UIImageView()
+        image.layer.cornerRadius = contentView.frame.width * 0.05
+        image.layer.masksToBounds = true
+        return image
+    }()
+    
+    private func setupDisplay(with model: CategoryCollectionViewCell) {
+        contentView.addSubview(categoryImage)
+        categoryImage.frame = contentView.frame
+    }
+    
+    func configure(with model: CategoryCollectionViewCell) {
+        categoryImage.image = UIImage(named: model.image)
+        setupDisplay(with: model)
+    }
+}
