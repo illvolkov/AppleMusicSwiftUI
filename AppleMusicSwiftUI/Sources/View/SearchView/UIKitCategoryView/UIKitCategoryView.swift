f5aeae98fbef3013a5e0f8c3da773c6b8414710a
Ilya Volkov <illvolkov@bk.ru>
2022-06-02 08:43:59 +0500

    Create UIKitCategoryView

---
+++ AppleMusicSwiftUI/Sources/View/SearchView/UIKitCategoryView/UIKitCategoryView.swift
@@ -0,0 +1,23 @@
+//
+//  UIKitCategoryView.swift
+//  AppleMusicSwiftUI
+//
+//  Created by Ilya Volkov on 02.06.2022.
+//
+
+import SwiftUI
+import UIKit
+
+struct UIKitCategoryView: UIViewControllerRepresentable {
+        
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let categoryController = CategoryController()
+        let navigationCategoryController = UINavigationController(rootViewController: categoryController)
+        
+        return navigationCategoryController
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+
+    }
+}
